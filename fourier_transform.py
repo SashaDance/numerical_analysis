c19f66529783c4cdf452cafa6b742bb4150afb2a
SashaDance <sksslr@mail.ru>
2023-11-14 18:48:57 +0300

    implemented fourier_transform

--- fourier_transform.py
+++ fourier_transform.py
@@ -1,9 +1,10 @@
 from matrix import Matrix
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft
+from scipy.fft import fft, ifft
 
 PI = np.pi
+
 
 def fourier_transform(x: np.ndarray[np.float64],
                       freq: int = 1000, n: int = 9,
@@ -43,14 +44,15 @@ def fourier_transform(x: np.ndarray[np.float64],
                     index2 = index1 + 2 ** (k - 1)
                     x1 = (X_fft[index1] +
                           np.exp(-2j * np.pi * l / (2 ** k)) * X_fft[index2]
-                    )
+                          )
                     x2 = (X_fft[index1] -
                           np.exp(-2j * np.pi * l / (2 ** k)) * X_fft[index2]
-                    )
+                          )
                     X_fft[index1] = x1
                     X_fft[index2] = x2
 
         return X_fft
+
 
 def tab(t: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
     np.random.seed(42)
@@ -61,9 +63,11 @@ def tab(t: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
             2 * np.random.randn(t.shape[0])
     )
 
+
 def spectral_density(X: np.ndarray[np.complex128]) -> np.ndarray[np.float64]:
     N = X.shape[0]
     return np.real(X * np.conj(X)) / N
+
 
 def plot_results(x: np.ndarray[np.float64], freq: float = 1000,
                  n: int = 9) -> None:
@@ -74,26 +78,64 @@ def plot_results(x: np.ndarray[np.float64], freq: float = 1000,
 
     X_fft_scipy = fft(x)
 
-    fig, axs = plt.subplots(1, 3)
-    axs[0].plot(
+    fig, axs = plt.subplots(3, 3)
+
+    # plotting the function, real and imaginary part of DFT
+    axs[0][0].plot(
+        freq * np.arange(0, N // 2) / N,
+        np.real(X_fft_scipy)[: N // 2]
+    )
+    axs[0][0].set_title('Real part of DFT')
+
+    axs[0][1].plot(
+        x[: N]
+    )
+    axs[0][1].set_title('Given Signal')
+
+    axs[0][2].plot(
+        freq * np.arange(0, N // 2) / N,
+        np.imag(X_fft_scipy)[: N // 2]
+    )
+    axs[0][2].set_title('Imaginary part of DFT')
+
+    # plotting DFT in 3 cases: implemented DFT and FFT, function from scipy lib
+    axs[1][0].plot(
         freq * np.arange(0, N // 2) / N,
         spectral_density(X_dft)[: N // 2]
     )
-    axs[0].set_title('Implemented DFT')
+    axs[1][0].set_title('Implemented DFT')
 
-    axs[1].plot(
+    axs[1][1].plot(
         freq * np.arange(0, N // 2) / N,
         spectral_density(X_fft)[: N // 2]
     )
-    axs[1].set_title('Implemented FFT')
+    axs[1][1].set_title('Implemented FFT')
 
-    axs[2].plot(
+    axs[1][2].plot(
         freq * np.arange(0, N // 2) / N,
         spectral_density(X_fft_scipy)[: N // 2]
     )
-    axs[2].set_title('Scipy FFT')
+    axs[1][2].set_title('Scipy FFT')
+
+    # plotting the inverse FFT in 3 cases
+
+    axs[2][0].plot(
+        np.real(ifft(X_dft))
+    )
+    axs[2][0].set_title('IFFT of implemented DFT')
+
+    axs[2][1].plot(
+        np.real(ifft(X_fft))
+    )
+    axs[2][1].set_title('IFFT of implemented FFT')
+
+    axs[2][2].plot(
+        np.real(ifft(X_fft_scipy))
+    )
+    axs[2][2].set_title('IFFT of scipy FFT')
 
     plt.show()
+
 
 if __name__ == '__main__':
     n = 9
