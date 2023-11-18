c29abcfe3f17bcc120c3c57ef87d44eb785165d8
SashaDance <sksslr@mail.ru>
2023-11-18 09:42:31 +0300

    spline interpolation implemented

--- spline_interpolation.py
+++ spline_interpolation.py
@@ -1,11 +1,13 @@
 import numpy as np
+import pandas as pd
 
 from matrix import Matrix
 from thomas_algorithm import thomas_algorithm
 import matplotlib.pyplot as plt
 from typing import Callable
 
-def predict(x: float, start: float, end: float, n:int, a: list[float],
+
+def predict(x: float, start: float, end: float, n: int, a: list[float],
             b: list[float], c: list[float], d: list[float]) -> float:
     h = (end - start) / n
 
@@ -16,13 +18,14 @@ def predict(x: float, start: float, end: float, n:int, a: list[float],
 
     residual = x - ind * h
     y = (a[ind] + b[ind] * residual
-                   + c[ind] * residual ** 2 + d[ind] * residual ** 3
+         + c[ind] * residual ** 2 + d[ind] * residual ** 3
     )
 
     return y
 
+
 def spline_interpolation(x: list[float], y: list[float],
-                         visualize: bool = False) -> None:
+                         visualize: bool = False) -> tuple:
     # initializing
     end = max(x)
     start = min(x)
@@ -57,8 +60,8 @@ def spline_interpolation(x: list[float], y: list[float],
     d = [0 for i in range(n)]
     for i in range(n):
         b[i] = (
-            (y[i + 1] - y[i]) / h
-            - h * (2 * c[i] + c[i + 1]) / 3
+                (y[i + 1] - y[i]) / h
+                - h * (2 * c[i] + c[i + 1]) / 3
         )
         d[i] = (c[i + 1] - c[i]) / (3 * h)
 
@@ -66,13 +69,15 @@ def spline_interpolation(x: list[float], y: list[float],
     x_sample = np.linspace(start, end, k)
     y_sample = np.zeros(100, dtype=np.float64)
     for i in range(k):
-        y_sample[i] = predict(x_sample[i], start, end, n, a, b, c, d)
+        y_sample[i] = predict(x_sample[i], start, end, n - 1, a, b, c, d)
 
     if visualize:
         plt.scatter(x, y, color='red')
         plt.plot(x_sample, y_sample)
 
         plt.show()
+
+    return a, b, c, d
 
 def function(x: float) -> float:
     return np.arcsin(2 * x - 1)
@@ -81,14 +86,36 @@ def function(x: float) -> float:
 def tab(func: Callable = function, l: float = 0, r: float = 1,
         n: int = 10) -> tuple[list[float], list[float]]:
     step = (r - l) / n
-    x_arr = list(np.arange(l, r, step))
+    x_arr = [l + (i - 0.5) * step for i in range(1, n + 1)]
     y_arr = list(map(func, x_arr))
 
     return x_arr, y_arr
 
+def print_results() -> None:
+    nums = [10, 25, 125]
+    for n in nums:
+        start = 0
+        end = 1
+        data = tab(n=n)
+        x = data[0]
+        y = data[1]
+        a, b, c, d = spline_interpolation(x, y)
+        y_pred = [
+            predict(x[i], start, end, n - 1, a, b, c, d) for i in range(n)
+        ]
 
-data = tab()
-x = data[0]
-y = data[1]
+        df_ = {
+            'x values': x,
+            'given function values': y,
+            'predicted values': y_pred,
+        }
 
-spline_interpolation(x, y, visualize=True)
+        df = pd.DataFrame(df_)
+
+        with pd.option_context('display.max_rows', None,
+                               'display.max_columns', None):
+            print(f'n = {n}')
+            print(df.to_string(index=False))
+
+
+print_results()
