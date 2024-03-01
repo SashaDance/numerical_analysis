3776470879e8a2e5b77c522b8881350d0b53a114
SashaDance <sksslr@mail.ru>
2024-03-01 16:33:24 +0300

    feat(thomas alg): code full refactored

--- thomas_algorithm.py
+++ thomas_algorithm.py
@@ -1,45 +1,51 @@
+import numpy as np
 
-def thomas_algorithm(kappa_1: float, kappa_2: float, a: list,
-                     b: list, c: list, f: list) -> list:
 
-    mu_1 = f[0]
-    alphas = [kappa_1]
-    betas = [mu_1]
-    n = len(f)
-    solution = [0 for i in range(n)]
+class ThomasAlg():
+    def __init__(self, matrix: list[list[float]], f: list[float]):
+        self.matrix = matrix
+        self.f = f
+        self.n = len(f)
+        assert self.n == np.sqrt(len(matrix)), f'Incorrect input'
+        self.__get_coefs()
+        self.alphas = [-self.c[0]]
+        self.betas = [f[0]]
 
-    # finding alphas and betas
-    for i in range(1, n - 1):
-        alpha = -c[i - 1] / (a[i - 1] * alphas[i - 1] + b[i - 1])
-        beta = (
-            (f[i] - a[i - 1] * betas[i - 1]) /
-            (a[i - 1] * alphas[i - 1] + b[i - 1])
+    def __get_coefs(self) -> None:
+        self.b = [self.matrix[i * self.n + i] for i in range(self.n)]
+        self.c = [self.matrix[i * self.n + i + 1] for i in range(self.n - 1)]
+        self.a = [self.matrix[(i + 1) * self.n + i] for i in range(self.n - 1)]
+
+    def solve(self):
+        for i in range(1, self.n - 1):
+            self.alphas.append(
+                -self.c[i] / (self.alphas[i - 1] * self.a[i - 1] + self.b[i])
+            )
+            self.betas.append(
+                (f[i] - self.a[i - 1] * self.betas[i - 1])
+                / (self.a[i - 1] * self.alphas[i - 1] + self.b[i])
+            )
+
+        ans = [0] * self.n
+        ans[-1] = (
+                (-self.a[-1] * self.betas[-1] + self.f[-1])
+                / (1 + self.a[-1] * self.alphas[-1])
         )
-        alphas.append(alpha)
-        betas.append(beta)
+        for i in range(self.n - 2, -1, -1):
+            ans[i] = self.alphas[i] * ans[i + 1] + self.betas[i]
 
-    mu_2 = f[n - 1]
-
-    # backwards
-    solution[n - 1] = (
-            (kappa_2 * betas[n - 2] + mu_2) /
-            (1 - kappa_2 * alphas[n - 2])
-    )
-
-    for i in range(n - 2, -1, -1):
-        solution[i] = alphas[i] * solution[i + 1] + betas[i]
-
-    return solution
+        return ans
 
 
-if __name__ == '__main__':
-    a = [-5, 4]
-    b = [-7, 4]
-    c = [2, -9]
-    f = [37, 17, 71, 51]
+matrix = [
+    2, -1, 0, 0,
+    1, 15, -2, 0,
+    0, -1, 3, 1,
+    0, 0, 1, 1
+]
+f = [-2, 38, 11, 6]
 
-    kappa_1 = -9
-    kappa_2 = -9
+inst = ThomasAlg(matrix, f)
+solution = inst.solve()
 
-    print('Solution')
-    print(thomas_algorithm(kappa_1, kappa_2, a, b, c, f))
+print(f'Solution:\n{solution}')
